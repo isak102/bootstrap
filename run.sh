32a2b1e28a854e81e39044678aea7f3fbdc4a090
Isak <isak.drevstad10@gmail.com>
2024-12-25 00:41:18 +0100

    feat: Ask for vault+become pass

--- run.sh
+++ run.sh
@@ -3,6 +3,21 @@ set -e
 
 DOTFILES_DIR="$HOME/.local/share/chezmoi"
 
-ansible-playbook -i ./ansible/inventory -vv --ask-vault-pass ./ansible/main.yml
+printf "Input BECOME (sudo) password: "
+read -s BECOME_PASS
+printf "\nEnter VAULT password: "
+read -s VAULT_PASS
+printf "\n"
 
-"$DOTFILES_DIR/setup/run.sh"
+VAULT_PASS_FILE="/tmp/vaultpass"
+BECOME_PASS_FILE="/tmp/becomepass"
+
+# TODO: Remove files when script exits regardless of success or failure
+echo "$VAULT_PASS" > $VAULT_PASS_FILE
+echo "$BECOME_PASS" > $BECOME_PASS_FILE
+
+ansible-playbook -i ./ansible/inventory -vv ./ansible/main.yml --vault-pass-file $VAULT_PASS_FILE --become-pass-file $BECOME_PASS_FILE
+
+"$DOTFILES_DIR/setup/run.sh" --vault-pass-file "$VAULT_PASS_FILE" --become-pass-file "$BECOME_PASS_FILE"
+
+rm -f $VAULT_PASS_FILE $BECOME_PASS_FILE
